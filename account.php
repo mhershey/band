cea2f27205b1df352e621a05f886786cb2c23cdc
Nicholas Lewis <nlewis@mail.umw.edu>
2010-02-25 16:46:37 -0500

    Added User Class

--- account.php
+++ account.php
@@ -1,43 +1,21 @@
-<?php include("header.php"); ?>
-
 <?php
+include_once('class_User.inc.php');
 $error = $user = $pass = "";
 
 if (isset($_POST['user']))
 {
-	
-	$user = str_replace("'","\'",$_POST['user']);
-	$pass = str_replace("'","\'",$_POST['pass']);
-	
-	if ($user == "" || $pass == "")
-	{
-		$error = "Not all fields were entered<br />";
-	}
-	else
-	{
-		$query = "SELECT user,pass FROM login
-				  WHERE user='$user' AND pass='$pass'";
-
-		if (mysqli_num_rows(mysqli_query($db,$query)) == 0)
-		{
-			$error = "Username/Password invalid<br />";
-		}
-		else
-		{
-			
-			$_SESSION['user'] = $user;
-			$_SESSION['pass'] = $pass;
-			echo "<h3>You are now logged in. Please
-			   <a href='index.php?view=$user'>click here</a>.</h3>";
-			
-		}
+	if(User::login($_POST['user'],$_POST['pass'])) {
+		header("Location: bands.php");
+	} else {
+		$error = "Error Logging In";
 	}
 }
-
+include("header.php");
 
 echo <<<_END
 <h2>Member Login</h2>
-<form method='post' action='account.php'><b>$error</b>
+<b>$error</b>
+<form method='post' action='account.php'>
 Username <input type='text' maxlength='16' name='user'
 	value='$user' /><br />
 Password <input type='password' maxlength='16' name='pass'
