dc423c0b7223f3ec496abe11fce525c40d7ee981
unknown <IMoore@.syntronics.local>
2010-03-16 14:51:13 -0400

    populates the venue table

---
+++ venuelist.sql
@@ -0,0 +1,22 @@
+--
+-- Insert some sample venues into the 'venue' table
+--
+
+
+INSERT INTO `venue` (`name`, `city`, `zipcode`, `description`, `musicType`) VALUES
+('Birchmere', 'Alexandria', 22305, 'One of the leading Folk clubs in the country. They also present some R&B/Bluegrass/Country/Fusion Jazz. Mike Jaworek books the club.', 'folk'),
+('Brittany's Restaurant & Sports Bar', 'Woodbridge', 22192, 'Rock and Roll, Classic Rock, Modern. They run the live music on Fridays, Saturdays and Special Events.', 'rock'),
+('Capital Alehouse', 'Richmond', 23219, 'It was built in the 1870s and we have restored the original hardwood floors, wooden rafters and old brick walls.', 'folk'),
+('Club Paradiso', 'Springfield', 22150, 'They present Country music & Jamie La Ritz is hosting an open mic.', 'country'),
+('Folk Club of Reston-Herndon', 'Herndon', 22070, 'An all-volunteer organization that promotes traditional and contemporary folk music and hosts an open mike every Tuesday.', 'folk'),
+('Jammin Java', 'Vienna', 22181, 'Variety of music from folk to alternative rock. National and regional acts.', 'rock'),
+('Jaxx', 'Springfield', 22152, 'Not really known for its cussine however you can occassionally see national Hard Rock and Metal acts that you won't find at other venues.', 'rock'),
+('Nissan Pavillion', 'Bristow', 20136, '25,000 seats makes this the largest outdoor amphitheater in the region.', 'misc'),
+('Old Brogue', 'Great Falls', 22066, 'A small pub with great food in the beautiful Great Falls area. Its nightly entertainment is Folk/Acoustic.', 'folk'),
+('Patriot Center', 'Alexandria', 22314, '10,000 plus or minus seats. Can be rented.', 'misc'),
+('Rhodeside Grill', 'Arlington', 22202, 'They present alternative rock and acoustic music.', 'alternative'),
+('Sala Thai', 'Arlington', 22202, 'Live jazz every Friday and Saturday nights. No cover charge for the music.', 'jazz'),
+('Secrets', 'Alexandria', 22309, 'Presents Rock/Pop mostly on weekends.', 'rock'),
+('Sign of the Whale', 'Falls Church', 22042, 'A restaurant/bar that presents Acoustic music.', 'acoustic'),
+('Sunset Grille', 'Annandale', 22003, 'Peanut shells litter the floor and some of the best Blues and Rockabilly sounds in town blast you out of your seat.', 'blues');
+
